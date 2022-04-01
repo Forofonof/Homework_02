e86e6b70ad435be9e84ae6c57bd990ebf516ca9f
Forofonof <DanielResult1@outlook.com>
2022-04-01 18:29:28 +0300

    Добавьте файлы проекта.

---
+++ Homework_02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше имя:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите вашу фамилию:");
+        string surname = Console.ReadLine();
+        Console.WriteLine("Ваше хобби?");
+        string hobby = Console.ReadLine();
+        Console.WriteLine("Какая у вас зарплата?");
+        string paycheck = Console.ReadLine();
+        Console.WriteLine($"Ваше имя: {name}. Ваша фамилия: {surname}. Ваше хобби: {hobby}. Ваш доход: {paycheck}.");
+    }
+}
